2aa773d658e1da563f0e7db32c83873bd741eca9
SergY29 <yurtin.sergey.v@gmail.com>
2023-02-26 12:20:43 +0200

    Update RegistrationScreen.js

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,18 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+    ImageBackground, StyleSheet, Text, TextInput, Button, Alert, View, KeyboardAvoidingView,
+    Platform, TouchableWithoutFeedback, Keyboard,
+} from 'react-native';
 
 export default function RegistrationScreen() {
+    const [name, setName] = useState("");
+    const [mail, setMail] = useState("");
+    const [password, setPassword] = useState("");
+    const [secureTextEntry, setsecureTextEntry] = useState(true);
+
+    const nameHandler = (text) => setName(text);
+    const mailHandler = (text) => setMail(text);
+    const passwordHandler = (text) => setPassword(text);
+
+    const onLogin = () => {
+        console.log(`Name: ${name} Email: ${mail} Password: ${password}`);
+        setName("");
+        setMail("");
+        setPassword("");
+    };
+
     return (
-        <View style={styles.container}>
-            <ImageBackground source={require('../assets/images/photo_bg.jpg')} style={styles.image}>
-                <StatusBar style="auto" />
-                <View style={styles.regContainer}>
-                    <Text>Open up App.js to start working on your RegistrationScreen!!</Text>
-                </View>
-
-
-            </ImageBackground>
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.container}>
+                <ImageBackground source={require('../assets/images/photo_bg.jpg')} style={styles.image}>
+                    <StatusBar style="auto" />
+                    <View style={styles.regContainer}>
+                        <Text style={styles.title}>Регистрация</Text>
+                        <View style={styles.form}>
+                            <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"}>
+                                <TextInput
+                                    value={name}
+                                    onChangeText={nameHandler}
+                                    placeholder="Логин"
+                                    style={styles.input}
+                                />
+                                <TextInput
+                                    value={mail}
+                                    onChangeText={mailHandler}
+                                    placeholder="Адрес электронной почты"
+                                    style={styles.input}
+                                />
+                                <TextInput
+                                    value={password}
+                                    onChangeText={passwordHandler}
+                                    placeholder="Пароль"
+                                    secureTextEntry={secureTextEntry}
+                                    style={styles.input}
+                                />
+                                <Button title={"Login"} style={styles.input} onPress={onLogin} />
+                            </KeyboardAvoidingView>
+                        </View>
+                    </View>
+                </ImageBackground>
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
 
@@ -26,11 +70,33 @@ const styles = StyleSheet.create({
         flex: 1,
         resizeMode: 'cover',
         justifyContent: 'flex-end',
+
+    },
+    title: {
+        fontSize: 30,
+        marginTop: 92,
+        marginBottom: 33,
     },
     regContainer: {
         flex: 0.7,
         backgroundColor: '#fff',
         borderTopLeftRadius: 20,
         borderTopRightRadius: 20,
-    }
+        alignItems: 'center',
+
+    },
+    form: {
+        width: "100%",
+        paddingLeft: 16,
+        paddingRight: 16,
+
+    },
+    input: {
+        height: 44,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "black",
+        marginBottom: 10,
+
+    },
 });
