0267d3f92fa65c9c5c8b552ddb1509598e6629e5
SergY29 <yurtin.sergey.v@gmail.com>
2023-03-01 22:36:58 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { useCallback } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
-SplashScreen.preventAutoHideAsync();
-
 import LoginScreen from './Screens/auth/LoginScreen';
 import RegistrationScreen from './Screens/auth/RegistrationScreen';
+
+
+SplashScreen.preventAutoHideAsync();
+
+const AuthStack = createStackNavigator();
+
+
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,15 +36,21 @@ export default function App() {
 
 
   return (
-    <View View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-      <LoginScreen />
-      {/* <RegistrationScreen /> */}
-      <StatusBar style="auto" />
-    </View >
+    <NavigationContainer onLayout={onLayoutRootView}>
+      <AuthStack.Navigator>
+        {/* <AuthStack.Screen name="Login" component={LoginScreen} /> */}
+        <AuthStack.Screen name="Registration" component={RegistrationScreen} />
+      </AuthStack.Navigator>
+    </NavigationContainer>
+
+
+
+
   );
 }
 
 
+    // <View View  >
 
 
 
