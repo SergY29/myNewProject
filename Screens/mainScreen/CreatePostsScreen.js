02c95ce7227e63c29b478607cd7ef95ff2eeaeb4
SergY29 <yurtin.sergey.v@gmail.com>
2023-03-03 15:53:16 +0200

    work 5.1

--- Screens/mainScreen/CreatePostsScreen.js
+++ Screens/mainScreen/CreatePostsScreen.js
@@ -1,10 +1,19 @@
 import { View, Text, StyleSheet } from "react-native";
+import { Camera } from "expo-camera";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+//icons
+import { AntDesign } from '@expo/vector-icons';
 
 
 export default function CreatePostsScreen() {
     return (
         <View style={styles.container}>
-            <Text>CreatePostsScreen</Text>
+            <Camera style={styles.camera}>
+                <TouchableOpacity style={styles.textCamera} onPress={() => { }}>
+                    <AntDesign name="camerao" size={35} color="white" />
+                </TouchableOpacity>
+            </Camera>
         </View>
     );
 };
@@ -12,9 +21,24 @@ export default function CreatePostsScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
         backgroundColor: "#fff",
     },
+    camera: {
+        height: 240,
+        marginTop: 32,
+        marginHorizontal: 16,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    textCamera: {
+        paddingTop: 13,
+        paddingBottom: 15,
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: "#fff",
+        backgroundColor: "rgba(255, 255, 255, 0.3)",
+    }
 });
 
