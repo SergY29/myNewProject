fd3bde28fcebb9b2bdea2b7ba87505490b47455b
SergY29 <yurtin.sergey.v@gmail.com>
2023-03-06 17:52:15 +0200

    firebase

---
+++ firebase/confi.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase";
+import 'firebase/auth';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJijf1Ff3KG9vurlUQ45wdq7vOoOq2RXc",
+    authDomain: "rn-social-1c302.firebaseapp.com",
+    projectId: "rn-social-1c302",
+    storageBucket: "rn-social-1c302.appspot.com",
+    messagingSenderId: "38956686340",
+    appId: "1:38956686340:web:7414c84f2ee197f4162951",
+    measurementId: "G-CE24G2FXF0"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
